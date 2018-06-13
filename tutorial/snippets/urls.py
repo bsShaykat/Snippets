89d7e38b4a5d92a6438c1f3e28b10e66d5a5a6be
bsShaykat <shaykat@bs-23.net>
2018-06-13 17:02:18 +0600

    hello world

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    url(r'^$', views.api_root),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-details'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-details'),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
