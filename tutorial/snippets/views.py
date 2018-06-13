591f1dabc704970c415ad21b0de61802f9b97126
bsShaykat <shaykat@bs-23.net>
2018-06-13 18:34:13 +0600

    view import change

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -6,7 +6,6 @@ from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework.response import Response
